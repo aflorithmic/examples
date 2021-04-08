2bb227ed96ee7881cfe06a9999d77df3739ee961
Antonio Tripiana Caballero <tonitripiana@gmail.com>
2021-04-08 11:01:29 +0200

    add welcome demo example

---
+++ python/welcome.py
@@ -0,0 +1,39 @@
+import aflr
+
+aflr.api_key = "your-key"  # or define env variable using export aflr_key=<your-key>
+text = "<<sectionName::welcome>> Hey {{username}}, welcome to my fitness app!"
+audience = [{"username": "John"}]
+
+# script creation
+script = aflr.Script().create(
+    scriptText=text,
+    projectName="examples",
+    moduleName="fitness",
+    scriptName="welcome-message",
+)
+print(f"Script created: \n {script} \n")
+
+# get the scriptId from the script created.
+scriptId = script["scriptId"]
+
+# speech creation
+response = aflr.Speech().create(
+    scriptId=scriptId, voice="Joanna", speed="110", audience=audience
+)
+print(f"Response from text-to-speech: \n {response} \n")
+
+# # mastering process
+response = aflr.Mastering().create(
+    scriptId=scriptId, backgroundTrackId="full__tropics.wav", audience=audience
+)
+print(f"Response from mastering: \n {response} \n")
+
+# # get url of audio tracks generated
+url = aflr.Mastering().retrieve(scriptId=scriptId, parameters=audience[0])
+print(f"url to download the track: \n {url} \n")
+
+# # or download
+file = aflr.Mastering().download(
+    scriptId=scriptId, parameters=audience[0], destination="."
+)
+print(f"file location: \n {file} \n")
